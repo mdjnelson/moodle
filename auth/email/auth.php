d6fc03acff26c9c733dae18b4d206cd61e5551b2
Mark Nelson <markn@moodle.com>
2013-11-16 15:30:52 -0800

    MDL-42934 auth_email: set the 'lang' for created users to the site setting

--- auth/email/auth.php
+++ auth/email/auth.php
@@ -90,6 +90,7 @@ class auth_plugin_email extends auth_plugin_base {
         require_once($CFG->dirroot.'/user/lib.php');
 
         $user->password = hash_internal_user_password($user->password);
+        $user->lang = $CFG->lang;
 
         $user->id = user_create_user($user, false);
 
