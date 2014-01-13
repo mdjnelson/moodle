3571c22649fd12927d97e84ed800bafccc8bd866
Mark Nelson <markn@moodle.com>
2014-01-12 18:41:18 -0800

    MDL-44316 core_tag: changed the API to accept a contextid and component

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -4197,7 +4197,7 @@ function delete_user(stdClass $user) {
     // TODO: remove from cohorts using standard API here.
 
     // Remove user tags.
-    tag_set('user', $user->id, array());
+    tag_set('user', $user->id, array(), 'core', $usercontext->id);
 
     // Unconditionally unenrol from all courses.
     enrol_user_delete($user);
