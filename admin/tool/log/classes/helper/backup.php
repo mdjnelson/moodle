6c3af644af5940120dbb2e1d069f3ab7e9272361
Mark Nelson <markn@moodle.com>
2014-07-25 14:18:36 -0700

    MDL-46455 tool_log: updated backup helper to support restoring

--- admin/tool/log/classes/helper/backup.php
+++ admin/tool/log/classes/helper/backup.php
@@ -34,11 +34,13 @@ defined('MOODLE_INTERNAL') || die();
  * @package    tool_log
  * @copyright  2014 Mark Nelson <markn@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * @property string $component Frankenstyle plugin name initialised in store trait.
  */
 trait backup {
 
     /**
-     * @var string $itembackup defines the type of logs we are backing up (if any) (eg. activity, course).
+     * @var string $itembackup defines the type of logs we are backing up (or restoring) (if any) (eg. activity, course).
      */
     private $itembackup;
 
@@ -69,10 +71,66 @@ trait backup {
     }
 
     public function restore_course_logs($step) {
+        if (!$step instanceof \restore_logs_structure_step) {
+            throw new \restore_step_exception('incorrect_step_type_passed_to_store');
+        }
 
+        // Set what we are restoring.
+        $this->itembackup = LOG_STORE_COURSE_LOGS;
+        $return = $step->execute_common_behaviour();
+        $this->itembackup = null;
+
+        return $return;
     }
 
     public function restore_activity_logs($step) {
+        if (!$step instanceof \restore_logs_structure_step) {
+            throw new \restore_step_exception('incorrect_step_type_passed_to_store');
+        }
 
+        // Set what we are restoring.
+        $this->itembackup = LOG_STORE_ACTIVITY_LOGS;
+        $return = $step->execute_common_behaviour();
+        $this->itembackup = null;
+
+        return $return;
+    }
+
+    /**
+     * Checks to see if this restore step should be executed.
+     *
+     * The function behaves the same as execute_condition() defined by the restore process in core.
+     *
+     * @see \restore_structure_step::execute_condition()
+     * @param \restore_logs_structure_step $step
+     * @return bool returns true if safe to execute, otherwise false
+     */
+    public function restore_execute_condition($step) {
+        // Check it is included in the backup.
+        $fullpath = $step->get_task()->get_taskbasepath();
+        $fullpath = rtrim($fullpath, '/') . '/' . $step->get_filename();
+        if (!file_exists($fullpath)) {
+            // Not found, can't restore course logs.
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns the structure to be processed by the \restore_step.
+     *
+     * The function behaves the same as define_structure() defined by the restore process in core.
+     *
+     * @see \restore_structure_step::define_structure
+     * @return \restore_path_element[] list of restore path elements
+     */
+    public function restore_define_structure() {
+        $paths = array();
+
+        // Simple, one plain level of information contains them.
+        $paths[] = new \restore_path_element('log', '/' . $this->component . '_logs/log');
+
+        return $paths;
     }
 }
