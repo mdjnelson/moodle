1195262c16258567402824661ad83dc4d4b05c35
Mark Nelson <markn@moodle.com>
2016-04-22 11:52:19 +0800

    Tasks

--- enrol/lti/classes/task/sync_grades.php
+++ enrol/lti/classes/task/sync_grades.php
@@ -50,7 +50,6 @@ class sync_grades extends \core\task\scheduled_task {
     public function execute() {
         global $DB, $CFG;
 
-        require_once($CFG->dirroot . '/enrol/lti/locallib.php');
         require_once($CFG->dirroot . '/enrol/lti/ims-blti/OAuth.php');
         require_once($CFG->dirroot . '/enrol/lti/ims-blti/OAuthBody.php');
         require_once($CFG->dirroot . '/lib/completionlib.php');
@@ -71,7 +70,7 @@ class sync_grades extends \core\task\scheduled_task {
         }
 
         // Get all the enabled tools.
-        if ($tools = enrol_lti_get_lti_tools(array('status' => ENROL_INSTANCE_ENABLED, 'gradesync' => 1))) {
+        if ($tools = \enrol_lti\helper::get_lti_tools(array('status' => ENROL_INSTANCE_ENABLED, 'gradesync' => 1))) {
             foreach ($tools as $tool) {
                 mtrace("Starting - Grade sync for shared tool '$tool->id' for the course '$tool->courseid'.");
 
@@ -80,99 +79,108 @@ class sync_grades extends \core\task\scheduled_task {
                 $sendcount = 0;
 
                 // We check for all the users - users can access the same tool from different consumers.
-                if ($users = $DB->get_records('enrol_lti_users', array('toolid' => $tool->id), 'lastaccess DESC')) {
+                if ($ltiusers = $DB->get_records('enrol_lti_users', array('toolid' => $tool->id), 'lastaccess DESC')) {
                     $completion = new \completion_info(get_course($tool->courseid));
-                    foreach ($users as $user) {
-                        $mtracecontent = "for the user '$user->userid' in the tool '$tool->id' for the course " .
+                    foreach ($ltiusers as $ltiuser) {
+                        $mtracecontent = "for the user '$ltiuser->userid' in the tool '$tool->id' for the course " .
                             "'$tool->courseid'";
 
                         $usercount = $usercount + 1;
-                        // This can happen is the sync process has an unexpected error.
-                        if (empty($user->serviceurl)) {
+
+                        // Check if we do not have a serviceurl - this can happen if the sync process has an unexpected error.
+                        if (empty($ltiuser->serviceurl)) {
                             mtrace("Skipping - Empty serviceurl $mtracecontent.");
                             continue;
                         }
-                        if (empty($user->sourceid)) {
+
+                        // Check if we do not have a sourceid - this can happen if the sync process has an unexpected error.
+                        if (empty($ltiuser->sourceid)) {
                             mtrace("Skipping - Empty sourceid $mtracecontent.");
                             continue;
                         }
 
+                        // Need a valid context to continue.
+                        if (!$context = \context::instance_by_id($tool->contextid)) {
+                            mtrace("Failed - Invalid contextid '$tool->contextid' for the tool '$tool->id'.");
+                            continue;
+                        }
+
+                        // Ok, let's get the grade.
                         $grade = false;
-                        if ($context = \context::instance_by_id($tool->contextid)) {
-                            if ($context->contextlevel == CONTEXT_COURSE) {
-                                // Check if the user did not completed the course when it was required.
-                                if ($tool->gradesynccompletion && !$completion->is_course_complete($user->userid)) {
-                                    mtrace("Skipping - Course not completed $mtracecontent.");
-                                    continue;
-                                }
-
-                                // Get the grade.
-                                if ($grade = grade_get_course_grade($user->userid, $tool->courseid)) {
-                                    $grademax = floatval($grade->item->grademax);
-                                    $grade = $grade->grade;
-                                }
-                            } else if ($context->contextlevel == CONTEXT_MODULE) {
-                                $cm = get_coursemodule_from_id(false, $context->instanceid, 0, false, MUST_EXIST);
-
-                                if ($tool->gradesynccompletion) {
-                                    $data = $completion->get_data($cm, false, $user->userid);
-                                    if ($data->completionstate != COMPLETION_COMPLETE_PASS &&
-                                        $data->completionstate != COMPLETION_COMPLETE) {
-                                        mtrace("Skipping - Activity not completed $mtracecontent.");
-                                        continue;
-                                    }
-                                }
-
-                                $grades = grade_get_grades($cm->course, 'mod', $cm->modname, $cm->instance, $user->userid);
-                                if (empty($grades->items[0]->grades)) {
-                                    $grade = false;
-                                } else {
-                                    $grade = reset($grades->items[0]->grades);
-                                    if (!empty($grade->item)) {
-                                        $grademax = floatval($grade->item->grademax);
-                                    } else {
-                                        $grademax = floatval($grades->items[0]->grademax);
-                                    }
-                                    $grade = $grade->grade;
-                                }
-                            }
-
-                            if ($grade === false || $grade === null || strlen($grade) < 1) {
-                                mtrace("Skipping - Invalid grade $mtracecontent.");
+                        if ($context->contextlevel == CONTEXT_COURSE) {
+                            // Check if the user did not completed the course when it was required.
+                            if ($tool->gradesynccompletion && !$completion->is_course_complete($ltiuser->userid)) {
+                                mtrace("Skipping - Course not completed $mtracecontent.");
                                 continue;
                             }
 
-                            // No need to be dividing by zero.
-                            if ($grademax == 0.0) {
-                                $grademax = 100.0;
+                            // Get the grade.
+                            if ($grade = grade_get_course_grade($ltiuser->userid, $tool->courseid)) {
+                                $grademax = floatval($grade->item->grademax);
+                                $grade = $grade->grade;
                             }
+                        } else if ($context->contextlevel == CONTEXT_MODULE) {
+                            $cm = get_coursemodule_from_id(false, $context->instanceid, 0, false, MUST_EXIST);
 
-                            // Sync with the external system.
-                            if ($grade != $user->lastgrade) {
-                                $floatgrade = $grade / $grademax;
-                                $body = enrol_lti_create_service_body($user->sourceid, $floatgrade);
-
-                                try {
-                                    $response = sendOAuthBodyPOST('POST', $user->serviceurl,
-                                        $user->consumerkey, $user->consumersecret, 'application/xml', $body);
-                                } catch (\Exception $e) {
-                                    mtrace($e->getMessage());
+                            if ($tool->gradesynccompletion) {
+                                $data = $completion->get_data($cm, false, $ltiuser->userid);
+                                if ($data->completionstate != COMPLETION_COMPLETE_PASS &&
+                                    $data->completionstate != COMPLETION_COMPLETE) {
+                                    mtrace("Skipping - Activity not completed $mtracecontent.");
                                     continue;
                                 }
-
-                                if (strpos(strtolower($response), 'success') !== false) {
-                                    $DB->set_field('enrol_lti_users', 'lastgrade', intval($grade), array('id' => $user->id));
-                                    mtrace("Success - The grade '$floatgrade' $mtracecontent was sent.");
-                                    $sendcount = $sendcount + 1;
-                                } else {
-                                    mtrace("Failed - The grade '$floatgrade' $mtracecontent failed to send.");
-                                }
-                            } else {
-                                mtrace("Not sent - The grade $mtracecontent was not sent as the grades are the same.");
                             }
-                        } else {
-                            mtrace("Failed - Invalid contextid '$tool->contextid' for the tool '$tool->id'.");
+
+                            $grades = grade_get_grades($cm->course, 'mod', $cm->modname, $cm->instance, $ltiuser->userid);
+                            if (!empty($grades->items[0]->grades)) {
+                                $grade = reset($grades->items[0]->grades);
+                                if (!empty($grade->item)) {
+                                    $grademax = floatval($grade->item->grademax);
+                                } else {
+                                    $grademax = floatval($grades->items[0]->grademax);
+                                }
+                                $grade = $grade->grade;
+                            }
                         }
+
+                        if ($grade === false || $grade === null || strlen($grade) < 1) {
+                            mtrace("Skipping - Invalid grade $mtracecontent.");
+                            continue;
+                        }
+
+                        // No need to be dividing by zero.
+                        if (empty($grademax)) {
+                            mtrace("Skipping - Invalid grade $mtracecontent.");
+                            continue;
+                        }
+
+                        // This can happen if the sync process has an unexpected error.
+                        if ($grade == $ltiuser->lastgrade) {
+                            mtrace("Not sent - The grade $mtracecontent was not sent as the grades are the same.");
+                            continue;
+                        }
+
+                        // Sync with the external system.
+                        $floatgrade = $grade / $grademax;
+                        $body = \enrol_lti\helper::create_service_body($ltiuser->sourceid, $floatgrade);
+
+                        try {
+                            $response = sendOAuthBodyPOST('POST', $ltiuser->serviceurl,
+                                $ltiuser->consumerkey, $ltiuser->consumersecret, 'application/xml', $body);
+                        } catch (\Exception $e) {
+                            mtrace("Failed - The grade '$floatgrade' $mtracecontent failed to send.");
+                            mtrace($e->getMessage());
+                            continue;
+                        }
+
+                        if (strpos(strtolower($response), 'success') !== false) {
+                            $DB->set_field('enrol_lti_users', 'lastgrade', intval($grade), array('id' => $ltiuser->id));
+                            mtrace("Success - The grade '$floatgrade' $mtracecontent was sent.");
+                            $sendcount = $sendcount + 1;
+                        } else {
+                            mtrace("Failed - The grade '$floatgrade' $mtracecontent failed to send.");
+                        }
+
                     }
                 }
                 mtrace("Completed - Synced grades for tool '$tool->id' in the course '$tool->courseid'. " .
--- enrol/lti/classes/task/sync_members.php
+++ enrol/lti/classes/task/sync_members.php
@@ -34,6 +34,16 @@ namespace enrol_lti\task;
 class sync_members extends \core\task\scheduled_task {
 
     /**
+     * The LTI message type.
+     */
+    const LTI_MESSAGE_TYPE = 'basic-lis-readmembershipsforcontext';
+
+    /**
+     * The LTI version.
+     */
+    const LTI_VERSION = 'LTI-1p0';
+
+    /**
      * Get a descriptive name for this task.
      *
      * @return string
@@ -50,7 +60,6 @@ class sync_members extends \core\task\scheduled_task {
     public function execute() {
         global $CFG, $DB;
 
-        require_once($CFG->dirroot . '/enrol/lti/locallib.php');
         require_once($CFG->dirroot . '/enrol/lti/ims-blti/OAuth.php');
         require_once($CFG->dirroot . '/enrol/lti/ims-blti/OAuthBody.php');
 
@@ -68,7 +77,7 @@ class sync_members extends \core\task\scheduled_task {
         }
 
         // Get all the enabled tools.
-        if ($tools = enrol_lti_get_lti_tools(array('status' => ENROL_INSTANCE_ENABLED, 'membersync' => 1))) {
+        if ($tools = \enrol_lti\helper::get_lti_tools(array('status' => ENROL_INSTANCE_ENABLED, 'membersync' => 1))) {
             $ltiplugin = enrol_get_plugin('lti');
             $consumers = array();
             $currentusers = array();
@@ -83,22 +92,25 @@ class sync_members extends \core\task\scheduled_task {
 
                 // We check for all the users - users can access the same tool from different consumers.
                 if ($ltiusers = $DB->get_records('enrol_lti_users', array('toolid' => $tool->id), 'lastaccess DESC')) {
-                    foreach ($ltiusers as $user) {
-                        $mtracecontent = "for the user '$user->userid' in the tool '$tool->id' for the course " .
+                    foreach ($ltiusers as $ltiuser) {
+                        $mtracecontent = "for the user '$ltiuser->userid' in the tool '$tool->id' for the course " .
                             "'$tool->courseid'";
                         $usercount++;
-                        if (!$user->membershipsurl) {
+
+                        // Check if we do not have a membershipsurl - this can happen if the sync process has an unexpected error.
+                        if (!$ltiuser->membershipsurl) {
                             mtrace("Skipping - Empty membershipsurl $mtracecontent.");
                             continue;
                         }
 
-                        if (!$user->membershipsid) {
+                        // Check if we do not have a membershipsid - this can happen if the sync process has an unexpected error.
+                        if (!$ltiuser->membershipsid) {
                             mtrace("Skipping - Empty membershipsid $mtracecontent.");
                             continue;
                         }
 
-                        $consumer = md5($user->membershipsurl . ':' . $user->membershipsid . ':' .
-                            $user->consumerkey . ':' . $user->consumersecret);
+                        $consumer = sha1($ltiuser->membershipsurl . ':' . $ltiuser->membershipsid . ':' .
+                            $ltiuser->consumerkey . ':' . $ltiuser->consumersecret);
                         if (in_array($consumer, $consumers)) {
                             // We have already synchronised with this consumer.
                             continue;
@@ -107,100 +119,101 @@ class sync_members extends \core\task\scheduled_task {
                         $consumers[] = $consumer;
 
                         $params = array(
-                            'lti_message_type' => 'basic-lis-readmembershipsforcontext',
-                            'id' => $user->membershipsid,
-                            'lti_version' => 'LTI-1p0'
+                            'lti_message_type' => self::LTI_MESSAGE_TYPE,
+                            'id' => $ltiuser->membershipsid,
+                            'lti_version' => self::LTI_VERSION
                         );
 
-                        mtrace("Calling memberships url '$user->membershipsurl' with body '" .
+                        mtrace("Calling memberships url '$ltiuser->membershipsurl' with body '" .
                             json_encode($params) . "'");
 
                         try {
-                            $response = sendOAuthParamsPOST('POST', $user->membershipsurl, $user->consumerkey,
-                                $user->consumersecret, 'application/x-www-form-urlencoded', $params);
+                            $response = sendOAuthParamsPOST('POST', $ltiuser->membershipsurl, $ltiuser->consumerkey,
+                                $ltiuser->consumersecret, 'application/x-www-form-urlencoded', $params);
                         } catch (\Exception $e) {
-                            mtrace("Exception: " . $e->getMessage());
-                            $response = false;
+                            mtrace("Skipping - No response received $mtracecontent from '$ltiuser->membershipsurl'");
+                            mtrace($e->getMessage());
+                            continue;
                         }
 
                         // Check the response from the consumer.
-                        if ($response) {
-                            $data = new \SimpleXMLElement($response);
+                        $data = new \SimpleXMLElement($response);
 
-                            if (!empty($data->statusinfo)) {
-                                if (strpos(strtolower($data->statusinfo->codemajor), 'success') !== false) {
-                                    $members = $data->memberships->member;
-                                    mtrace(count($members) . ' members received.');
-                                    foreach ($members as $member) {
-                                        // Set the user data.
-                                        $user = new \stdClass();
-                                        $user->username = enrol_lti_create_username($user->consumerkey, $member->user_id);
-                                        $user->firstname = clean_param($member->person_name_given, PARAM_TEXT);
-                                        $user->lastname = clean_param($member->person_name_family, PARAM_TEXT);
-                                        $user->email = clean_param($member->person_contact_email_primary, PARAM_EMAIL);
+                        // Check if we did not receive a valid response.
+                        if (empty($data->statusinfo)) {
+                            mtrace("Skipping - Bad response received $mtracecontent from '$ltiuser->membershipsurl'");
+                            mtrace('Skipping - Error parsing the XML received \'' . substr($response, 0, 125) .
+                                '\' ... (Displaying only 125 chars)');
+                            continue;
+                        }
 
-                                        // Get the user data from the LTI consumer.
-                                        $user = enrol_lti_assign_user_tool_data($tool, $user);
+                        // Check if we did not receive a valid response.
+                        if (strpos(strtolower($data->statusinfo->codemajor), 'success') === false) {
+                            mtrace('Skipping - Error received from the remote system: ' . $data->statusinfo->codemajor
+                                . ' ' . $data->statusinfo->severity . ' ' . $data->statusinfo->codeminor);
+                            continue;
+                        }
 
-                                        if (!$dbuser = $DB->get_record('user', array('username' => $user->username,
-                                            'deleted' => 0))) {
-                                            if ($tool->membersyncmode == ENROL_LTI_MEMBER_SYNC_ENROL_AND_UNENROL ||
-                                                $tool->membersyncmode == ENROL_LTI_MEMBER_SYNC_ENROL_NEW) {
-                                                // If the email was stripped/not set then fill it with a default one. This
-                                                // stops the user from being redirected to edit their profile page.
-                                                if (empty($user->email)) {
-                                                    $user->email = $user->username .  "@example.com";
-                                                }
+                        $members = $data->memberships->member;
+                        mtrace(count($members) . ' members received.');
+                        foreach ($members as $member) {
+                            // Set the user data.
+                            $user = new \stdClass();
+                            $user->username = \enrol_lti\helper::create_username($ltiuser->consumerkey, $member->user_id);
+                            $user->firstname = clean_param($member->person_name_given, PARAM_TEXT);
+                            $user->lastname = clean_param($member->person_name_family, PARAM_TEXT);
+                            $user->email = clean_param($member->person_contact_email_primary, PARAM_EMAIL);
 
-                                                $user->auth = 'lti';
-                                                $user->id = user_create_user($user);
+                            // Get the user data from the LTI consumer.
+                            $user = \enrol_lti\helper::assign_user_tool_data($tool, $user);
 
-                                                // Add the information to the necessary arrays.
-                                                $currentusers[] = $user->id;
-                                                $userphotos[$user->id] = $member->user_image;
-                                            }
-                                        } else {
-                                            // If email is empty remove it, so we don't update the user with an empty email.
-                                            if (empty($user->email)) {
-                                                unset($user->email);
-                                            }
-
-                                            $user->id = $dbuser->id;
-                                            user_update_user($user);
-
-                                            // Add the information to the necessary arrays.
-                                            $currentusers[] = $user->id;
-                                            $userphotos[$user->id] = $member->user_image;
-                                        }
-                                        if ($tool->membersyncmode == ENROL_LTI_MEMBER_SYNC_ENROL_AND_UNENROL ||
-                                            $tool->membersyncmode == ENROL_LTI_MEMBER_SYNC_ENROL_NEW) {
-                                            // Enrol the user in the course.
-                                            enrol_lti_enrol_user($tool, $user->id);
-                                        }
+                            if (!$dbuser = $DB->get_record('user', array('username' => $user->username, 'deleted' => 0))) {
+                                if ($tool->membersyncmode == \enrol_lti\helper::MEMBER_SYNC_ENROL_AND_UNENROL ||
+                                    $tool->membersyncmode == \enrol_lti\helper::MEMBER_SYNC_ENROL_NEW) {
+                                    // If the email was stripped/not set then fill it with a default one. This
+                                    // stops the user from being redirected to edit their profile page.
+                                    if (empty($user->email)) {
+                                        $user->email = $user->username .  "@example.com";
                                     }
-                                } else {
-                                    mtrace('Error received from the remote system: ' . $data->statusinfo->codemajor
-                                        . ' ' . $data->statusinfo->severity . ' ' . $data->statusinfo->codeminor);
+
+                                    $user->auth = 'lti';
+                                    $user->id = user_create_user($user);
+
+                                    // Add the information to the necessary arrays.
+                                    $currentusers[] = $user->id;
+                                    $userphotos[$user->id] = $member->user_image;
                                 }
                             } else {
-                                mtrace('Error parsing the XML received \'' . substr($response, 0, 125) .
-                                    '\' ... (Displaying only 125 chars)');
+                                // If email is empty remove it, so we don't update the user with an empty email.
+                                if (empty($user->email)) {
+                                    unset($user->email);
+                                }
+
+                                $user->id = $dbuser->id;
+                                user_update_user($user);
+
+                                // Add the information to the necessary arrays.
+                                $currentusers[] = $user->id;
+                                $userphotos[$user->id] = $member->user_image;
                             }
-                        } else {
-                            mtrace('No response received from \'' . $user->membershipsurl . '\'');
+                            if ($tool->membersyncmode == \enrol_lti\helper::MEMBER_SYNC_ENROL_AND_UNENROL ||
+                                $tool->membersyncmode == \enrol_lti\helper::MEMBER_SYNC_ENROL_NEW) {
+                                // Enrol the user in the course.
+                                \enrol_lti\helper::enrol_user($tool, $user->id);
+                            }
                         }
                     }
                     // Now we check if we have to unenrol users who were not listed.
-                    if ($tool->membersyncmode == ENROL_LTI_MEMBER_SYNC_ENROL_AND_UNENROL ||
-                        $tool->membersyncmode == ENROL_LTI_MEMBER_SYNC_UNENROL_MISSING) {
+                    if ($tool->membersyncmode == \enrol_lti\helper::MEMBER_SYNC_ENROL_AND_UNENROL ||
+                        $tool->membersyncmode == \enrol_lti\helper::MEMBER_SYNC_UNENROL_MISSING) {
                         // Go through the users and check if any were never listed, if so, remove them.
-                        foreach ($ltiusers as $user) {
-                            if (!in_array($user->userid, $currentusers)) {
+                        foreach ($ltiusers as $ltiuser) {
+                            if (!in_array($ltiuser->userid, $currentusers)) {
                                 $instance = new \stdClass();
                                 $instance->id = $tool->enrolid;
                                 $instance->courseid = $tool->courseid;
                                 $instance->enrol = 'lti';
-                                $ltiplugin->unenrol_user($instance, $user->id);
+                                $ltiplugin->unenrol_user($instance, $ltiuser->id);
                             }
                         }
                     }
@@ -216,8 +229,8 @@ class sync_members extends \core\task\scheduled_task {
             if (!empty($userphotos)) {
                 foreach ($userphotos as $userid => $url) {
                     if ($url) {
-                        $result = enrol_lti_update_user_profile_image($userid, $url);
-                        if ($result === true) {
+                        $result = \enrol_lti\helper::update_user_profile_image($userid, $url);
+                        if ($result === \enrol_lti\helper::PROFILE_IMAGE_UPDATE_SUCCESSFUL) {
                             $counter++;
                             mtrace("Profile image succesfully downloaded and created for user '$userid' from $url.");
                         } else {
