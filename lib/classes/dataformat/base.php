9902050c842ec81e95bd5f7f1b32f29e4111fac8
Mark Nelson <markn@moodle.com>
2017-05-24 23:11:38 +0800

    MDL-56046 core_dataformat: added functions to support multiple sheets
    Also removed write_header() and write_footer(). The reason for this
    is because in core we want to know if the format being used supports
    multiple sheets. To do this, and ensure we do not break third-party
    dataformat plugins, we are using method_exist(). If write_header()
    and write_footer() remain in the base class then this will always
    be true.

--- lib/classes/dataformat/base.php
+++ lib/classes/dataformat/base.php
@@ -98,11 +98,18 @@ abstract class base {
     }
 
     /**
-     * Write the start of the format
+     * Write the start of the file.
+     */
+    public function start_output() {
+        // Override me if needed.
+    }
+
+    /**
+     * Write the start of the sheet we will be adding data to.
      *
      * @param array $columns
      */
-    public function write_header($columns) {
+    public function start_sheet($columns) {
         // Override me if needed.
     }
 
@@ -115,12 +122,18 @@ abstract class base {
     abstract public function write_record($record, $rownum);
 
     /**
-     * Write the end of the format
+     * Write the end of the sheet containing the data.
      *
      * @param array $columns
      */
-    public function write_footer($columns) {
+    public function close_sheet($columns) {
         // Override me if needed.
     }
 
+    /**
+     * Write the end of the file.
+     */
+    public function close_output() {
+        // Override me if needed.
+    }
 }
