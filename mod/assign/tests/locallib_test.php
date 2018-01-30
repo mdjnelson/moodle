74b16076f9c85a796fb21fbf4777ab0908400549
Mark Nelson <markn@moodle.com>
2018-01-30 18:26:27 +0800

    MDL-36941 core: added events for notifications

--- mod/assign/tests/locallib_test.php
+++ mod/assign/tests/locallib_test.php
@@ -1290,12 +1290,12 @@ class mod_assign_locallib_testcase extends mod_assign_base_testcase {
         $events = $sink->get_events();
         // Two messages are sent, one to student and one to teacher. This generates
         // four events:
-        // core\event\message_sent
-        // core\event\message_viewed
-        // core\event\message_sent
-        // core\event\message_viewed.
+        // core\event\notification_sent
+        // core\event\notificiation_viewed
+        // core\event\notification_sent
+        // core\event\notification_viewed.
         $event = reset($events);
-        $this->assertInstanceOf('\core\event\message_sent', $event);
+        $this->assertInstanceOf('\core\event\notification_sent', $event);
         $this->assertEquals($assign->get_course()->id, $event->other['courseid']);
         $sink->close();
     }
