1042d45b947eeed379f70226d4bd9838fe0a11b5
Mark Nelson <markn@moodle.com>
2019-01-07 16:55:58 +0800

    MDL-62599 mod_lti: auth cleanup

--- mod/lti/auth.php
+++ mod/lti/auth.php
@@ -25,107 +25,99 @@
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->dirroot . '/mod/lti/locallib.php');
 
-$scope = optional_param('scope', '', PARAM_TEXT);
-$responsetype = optional_param('response_type', '', PARAM_TEXT);
-$clientid = optional_param('client_id', '', PARAM_TEXT);
-$redirecturi = optional_param('redirect_uri', '', PARAM_TEXT);
-$loginhint = optional_param('login_hint', '', PARAM_TEXT);
+$scope = required_param('scope', PARAM_TEXT);
+$responsetype = required_param('response_type', PARAM_TEXT);
+$clientid = required_param('client_id', PARAM_TEXT);
+$redirecturi = required_param('redirect_uri', PARAM_TEXT);
+$loginhint = required_param('login_hint', PARAM_TEXT);
+$nonce = required_param('nonce', PARAM_TEXT);
 $state = optional_param('state', '', PARAM_TEXT);
 $responsemode = optional_param('response_mode', '', PARAM_TEXT);
-$nonce = optional_param('nonce', '', PARAM_TEXT);
 $prompt = optional_param('prompt', '', PARAM_TEXT);
 
-$ok = !empty($scope) && !empty($responsetype) && !empty($clientid) && !empty($redirecturi) && !empty($loginhint) && !empty($scope) &&
-      !empty($nonce) && !empty($SESSION->lti_message_hint);
-
-if (!$ok) {
-    $error = 'invalid_request';
-}
-if ($ok && ($scope !== 'openid')) {
-    $ok = false;
-    $error = 'invalid_scope';
-}
-if ($ok && ($responsetype !== 'id_token')) {
-    $ok = false;
-    $error = 'unsupported_response_type';
-}
-if ($ok) {
-    list($courseid, $typeid, $id, $titleb64, $textb64, $targeturib64) = explode(',', $SESSION->lti_message_hint, 6);
-    $config = lti_get_type_type_config($typeid);
-    $ok = ($clientid === $config->lti_clientid);
-    if (!$ok) {
-        $error = 'unauthorized_client';
-    }
-}
-if ($ok && ($loginhint !== $USER->id)) {
-    $ok = false;
-    $error = 'access_denied';
-}
-if ($ok) {
-    $uris = array_map("trim", explode("\n", $config->lti_redirectionuris));
-    $ok = in_array($redirecturi, $uris);
-    if (!$ok) {
-        $error = 'invalid_request';
-        $desc = 'Unregistered redirect_uri ' . $redirecturi;
-    }
-}
-if ($ok) {
-    if (isset($responsemode)) {
-        $ok = ($responsemode === 'form_post');
-        if (!$ok) {
-            $error = 'invalid_request';
-            $desc = 'Invalid response_mode';
-        }
-    } else {
-        $responsemode = 'query';
-    }
-}
-if ($ok && !empty($prompt) && ($prompt !== 'none')) {
-    $ok = false;
-    $error = 'invalid_request';
-    $desc = 'Invalid prompt';
+if (empty($SESSION->lti_message_hint)) {
+    throw new moodle_exception('invalid_request');
 }
 
-if ($ok) {
-    $course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
-    if ($id) {
-        $cm = get_coursemodule_from_id('lti', $id, 0, false, MUST_EXIST);
-        $context = context_module::instance($cm->id);
-        require_login($course, true, $cm);
-        require_capability('mod/lti:view', $context);
-        $lti = $DB->get_record('lti', array('id' => $cm->instance), '*', MUST_EXIST);
-        list($endpoint, $params) = lti_get_launch_data($lti, $nonce);
-    } else {
-        require_login($course);
-        $context = context_course::instance($courseid);
-        require_capability('moodle/course:manageactivities', $context);
-        require_capability('mod/lti:addcoursetool', $context);
-        // Set the return URL. We send the launch container along to help us avoid frames-within-frames when the user returns.
-        $returnurlparams = [
-            'course' => $courseid,
-            'id' => $typeid,
-            'sesskey' => sesskey()
-        ];
-        $returnurl = new \moodle_url('/mod/lti/contentitem_return.php', $returnurlparams);
-        // Prepare the request.
-        $title = base64_decode($titleb64);
-        $text = base64_decode($textb64);
-        $targeturi = base64_decode($targeturib64);
-        $request = lti_build_content_item_selection_request($typeid, $course, $returnurl, $title, $text, $targeturi, [], [], false, false, false, false, false, $nonce);
-        $endpoint = $request->url;
-        $params = $request->params;
+if ($scope !== 'openid') {
+    throw new moodle_exception('invalid_scope');
+}
+
+if ($responsetype !== 'id_token') {
+    throw new moodle_exception('unsupported_response_type');
+}
+
+list($courseid, $typeid, $id, $titleb64, $textb64, $targeturib64) = explode(',', $SESSION->lti_message_hint, 6);
+$config = lti_get_type_type_config($typeid);
+
+if ($clientid !== $config->lti_clientid) {
+    throw new moodle_exception('unauthorized_client');
+}
+
+if ($loginhint !== $USER->id) {
+    throw new moodle_exception('access_denied');
+}
+
+$uris = array_map("trim", explode("\n", $config->lti_redirectionuris));
+
+if (!in_array($redirecturi, $uris)) {
+    throw new moodle_exception('invalid_request');
+}
+
+if (!empty($responsemode)) {
+    if ($responsemode !== 'form_post') {
+        throw new moodle_exception('invalid_request');
     }
 } else {
-    $params['error'] = $error;
-    if (!empty($desc)) {
-        $params['error_description'] = $desc;
-    }
+    $responsemode = 'query';
 }
-if (isset($state)) {
+
+if (!empty($prompt) && $prompt !== 'none') {
+    throw new moodle_exception('invalid_request');
+}
+
+$course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
+if ($id) {
+    $cm = get_coursemodule_from_id('lti', $id, 0, false, MUST_EXIST);
+    $context = context_module::instance($cm->id);
+
+    require_login($course, true, $cm);
+    require_capability('mod/lti:view', $context);
+
+    $lti = $DB->get_record('lti', array('id' => $cm->instance), '*', MUST_EXIST);
+    list($endpoint, $params) = lti_get_launch_data($lti, $nonce);
+} else {
+    require_login($course);
+
+    $context = context_course::instance($courseid);
+    require_capability('moodle/course:manageactivities', $context);
+    require_capability('mod/lti:addcoursetool', $context);
+
+    // Set the return URL. We send the launch container along to help us avoid frames-within-frames when the user returns.
+    $returnurlparams = [
+        'course' => $courseid,
+        'id' => $typeid,
+        'sesskey' => sesskey()
+    ];
+
+    $returnurl = new \moodle_url('/mod/lti/contentitem_return.php', $returnurlparams);
+
+    // Prepare the request.
+    $title = base64_decode($titleb64);
+    $text = base64_decode($textb64);
+    $targeturi = base64_decode($targeturib64);
+    $request = lti_build_content_item_selection_request($typeid, $course, $returnurl, $title, $text, $targeturi, [], [], false, false, false, false, false, $nonce);
+    $endpoint = $request->url;
+    $params = $request->params;
+}
+
+if (!empty($state)) {
     $params['state'] = $state;
 }
+
 if ($responsemode !== 'query') {
-    $r = '<form action="' . $redirecturi . "\" name=\"ltiAuthForm\" id=\"ltiAuthForm\" method=\"post\" enctype=\"application/x-www-form-urlencoded\">\n";
+    $r = "<form action='" . $redirecturi . "' name='ltiAuthForm' id='ltiAuthForm' " .
+        " method=\"post\" enctype=\"application/x-www-form-urlencoded\">\n";
     if (!empty($params)) {
         foreach ($params as $key => $value) {
             $key = htmlspecialchars($key);
@@ -141,8 +133,6 @@ if ($responsemode !== 'query') {
         "</script>\n";
     echo $r;
 } else {
-    $url = new \moodle_url(redirecturi, $params);
+    $url = new \moodle_url($redirecturi, $params);
     redirect($url->out(false));
 }
-
-?>
